786e479ea400510938552c42afe6cfa42e1420f4
Oytizzo <oytizzo@gmail.com>
2021-11-20 18:24:28 +0600

    Done Data List

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,6 @@
 from django.views import generic
 
-from product.models import Variant
+from product.models import Variant, Product, ProductVariantPrice
 
 
 class CreateProductView(generic.TemplateView):
@@ -12,3 +12,17 @@ class CreateProductView(generic.TemplateView):
         context['product'] = True
         context['variants'] = list(variants.all())
         return context
+
+
+class ProductListView(generic.ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = 'products/list.html'
+    paginate_by = 10
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['variants']
+    #     product_variant_price = ProductVariantPrice.objects.first()
+    #     context['products'] = products
+    #     context['product_variant_price'] = product_variant_price
